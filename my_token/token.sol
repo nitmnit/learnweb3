6c2729f6486b0b951dd68b0ce202b621f6616c2d
Nitin Suresh <nitin.kalal@ibigroup.com>
2023-06-03 05:09:47 +0000

    fixed token contract

--- my_token/token.sol
+++ my_token/token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+/// @custom:dev-run-script scripts/deploy_with_ethers.ts
 contract PrivateToken is ERC20 {
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
         _mint(msg.sender, 10 * 10 ** 18);
